9c42f08bbb2baae2b0d7529b7a9ac0931d21ed1b
Chrisaly <christophelndong@gmail.com>
2023-03-23 03:14:43 +0100

    Recherche de racines

---
+++ recherche_incrementale.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 23 01:27:32 2023
+
+@author: christophel
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+""" Méthode de recherche incrémentale dans la recherche de racines"""
+def recherche_incrementale(f, a, b, dx):
+        
+    """
+    f : La fonction à résoudre
+    a : La valeur de l'axe des x de la frontière gauche
+    b : La valeur de l'axe des x de la frontière droite
+    dx : La valeur incrémentale dans la recherche
+    return : La valeur de l'axe des x de la racine,
+    le nombre d'itérations utilisées"""
+    
+    fa = f(a)
+    c = a + dx
+    fc = f(c)
+    n = 1
+    while np.sign(fa) == np.sign(fc):
+        if a >= b:
+            return a - dx, n
+        a = c
+        fa = fc
+        c = a + dx
+        fc = f(c)
+        n += 1
+    if fa == 0:
+        return a, n
+    elif fc == 0:
+        return c, n
+    else:
+        return (a + c)/2., n
+    
+#y = lambda x: x**3 + 2.0*x**2 - 5.
+#racine,iterations = recherche_incrementale(y, -5., 5., 0.001)
